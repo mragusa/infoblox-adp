985107d273767df61273de4ff14b2c10a6f65416
Michael Ragusa <mragusa@infoblox.com>
2024-01-30 00:09:39 -0500

    applied style code

--- adp-tunnel-ruleset.py
+++ adp-tunnel-ruleset.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import urllib3
+
 urllib3.disable_warnings()
 import re
 from infoblox_client import connector
@@ -38,7 +39,7 @@ for n in grid_tp:
     print("Grid Name: {}".format(n["grid_name"]))
     print("Grid Ruleset: {}".format(n["current_ruleset"]))
     if "scheduled_download" in n:
-     	print("Grid Scheduled Download: {}".format(n["scheduled_download"]))
+        print("Grid Scheduled Download: {}".format(n["scheduled_download"]))
 grid_tp_ruleset = conn.get_object(
     "threatprotection:ruleset",
     return_fields=["used_by", "version", "add_type", "comment"],
@@ -67,15 +68,63 @@ for mtpr in grid_tp_rules:
         # check if rule is disabled or not
         if mtpr["disable"] is True:
             if mtpr["config"]["log_severity"] == "INFORMATIONAL":
-                print( "\033[94mMember\033[00m: \033[96m{}\033[00m, \033[94mRule\033[00m: {}, \033[94mConfig\033[00m: {} ,\033[94mSeverity\033[00m: \033[93m{}\033[00m, \033[94mDisabled\033[00m: \033[91m{}\033[00m".format( mtpr["member"], mtpr["rule"], mtpr["config"]["action"],mtpr["config"]["log_severity"], mtpr["disable"]))
+                print(
+                    "\033[94mMember\033[00m: \033[96m{}\033[00m, \033[94mRule\033[00m: {}, \033[94mConfig\033[00m: {} ,\033[94mSeverity\033[00m: \033[93m{}\033[00m, \033[94mDisabled\033[00m: \033[91m{}\033[00m".format(
+                        mtpr["member"],
+                        mtpr["rule"],
+                        mtpr["config"]["action"],
+                        mtpr["config"]["log_severity"],
+                        mtpr["disable"],
+                    )
+                )
             elif mtpr["config"]["log_severity"] == "MAJOR":
-                print( "\033[94mMember\033[00m: \033[96m{}\033[00m, \033[94mRule\033[00m: {}, \033[94mConfig\033[00m: {} ,\033[94mSeverity\033[00m: \033[91m{}\033[00m, \033[94mDisabled\033[00m: \033[91m{}\033[00m".format( mtpr["member"], mtpr["rule"], mtpr["config"]["action"],mtpr["config"]["log_severity"], mtpr["disable"]))
+                print(
+                    "\033[94mMember\033[00m: \033[96m{}\033[00m, \033[94mRule\033[00m: {}, \033[94mConfig\033[00m: {} ,\033[94mSeverity\033[00m: \033[91m{}\033[00m, \033[94mDisabled\033[00m: \033[91m{}\033[00m".format(
+                        mtpr["member"],
+                        mtpr["rule"],
+                        mtpr["config"]["action"],
+                        mtpr["config"]["log_severity"],
+                        mtpr["disable"],
+                    )
+                )
             else:
-                print( "\033[94mMember\033[00m: \033[96m{}\033[00m, \033[94mRule\033[00m: {}, \033[94mConfig\033[00m: {} ,\033[94mSeverity\033[00m: {}, \033[94mDisabled\033[00m: \033[91m{}\033[00m".format( mtpr["member"], mtpr["rule"], mtpr["config"]["action"],mtpr["config"]["log_severity"], mtpr["disable"]))
+                print(
+                    "\033[94mMember\033[00m: \033[96m{}\033[00m, \033[94mRule\033[00m: {}, \033[94mConfig\033[00m: {} ,\033[94mSeverity\033[00m: {}, \033[94mDisabled\033[00m: \033[91m{}\033[00m".format(
+                        mtpr["member"],
+                        mtpr["rule"],
+                        mtpr["config"]["action"],
+                        mtpr["config"]["log_severity"],
+                        mtpr["disable"],
+                    )
+                )
         else:
             if mtpr["config"]["log_severity"] == "INFORMATIONAL":
-                print( "\033[94mMember\033[00m: \033[96m{}\033[00m, \033[94mRule\033[00m: {}, \033[94mConfig\033[00m: {} ,\033[94mSeverity\033[00m: \033[93m{}\033[00m, \033[94mDisabled\033[00m: {}".format( mtpr["member"], mtpr["rule"], mtpr["config"]["action"],mtpr["config"]["log_severity"], mtpr["disable"]))
+                print(
+                    "\033[94mMember\033[00m: \033[96m{}\033[00m, \033[94mRule\033[00m: {}, \033[94mConfig\033[00m: {} ,\033[94mSeverity\033[00m: \033[93m{}\033[00m, \033[94mDisabled\033[00m: {}".format(
+                        mtpr["member"],
+                        mtpr["rule"],
+                        mtpr["config"]["action"],
+                        mtpr["config"]["log_severity"],
+                        mtpr["disable"],
+                    )
+                )
             elif mtpr["config"]["log_severity"] == "MAJOR":
-                print( "\033[94mMember\033[00m: \033[96m{}\033[00m, \033[94mRule\033[00m: {}, \033[94mConfig\033[00m: {} ,\033[94mSeverity\033[00m: \033[91m{}\033[00m, \033[94mDisabled\033[00m: {}".format( mtpr["member"], mtpr["rule"], mtpr["config"]["action"],mtpr["config"]["log_severity"], mtpr["disable"]))
+                print(
+                    "\033[94mMember\033[00m: \033[96m{}\033[00m, \033[94mRule\033[00m: {}, \033[94mConfig\033[00m: {} ,\033[94mSeverity\033[00m: \033[91m{}\033[00m, \033[94mDisabled\033[00m: {}".format(
+                        mtpr["member"],
+                        mtpr["rule"],
+                        mtpr["config"]["action"],
+                        mtpr["config"]["log_severity"],
+                        mtpr["disable"],
+                    )
+                )
             else:
-                print( "\033[94mMember\033[00m: \033[96m{}\033[00m, \033[94mRule\033[00m: {}, \033[94mConfig\033[00m: {} \033[94mSeverity\033[00m: {}, \033[94mDisabled\033[00m: {}".format( mtpr["member"], mtpr["rule"], mtpr["config"]["action"],mtpr["config"]["log_severity"], mtpr["disable"]))
+                print(
+                    "\033[94mMember\033[00m: \033[96m{}\033[00m, \033[94mRule\033[00m: {}, \033[94mConfig\033[00m: {} \033[94mSeverity\033[00m: {}, \033[94mDisabled\033[00m: {}".format(
+                        mtpr["member"],
+                        mtpr["rule"],
+                        mtpr["config"]["action"],
+                        mtpr["config"]["log_severity"],
+                        mtpr["disable"],
+                    )
+                )

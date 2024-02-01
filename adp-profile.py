0ab70069ac96dbf60e6836d43cecf4cb4152d947
Michael Ragusa <mragusa@infoblox.com>
2024-02-01 01:17:18 -0500

    added create, remove functinality and lines to grab current rulesets for profile creation

--- adp-profile.py
+++ adp-profile.py
@@ -11,16 +11,15 @@ import argparse
 parser = argparse.ArgumentParser(
     prog="Infoblox ADP Framework Profile Interactions",
     description="Create/Remove ADP Profiles - Assign/Remove members to profiles",
-    epilog="ADP Profiles are used to create customer rulesets for enterprise clients",
+    epilog="ADP Profiles are used to create custom rulesets for enterprise clients",
 )
 parser.add_argument("gmhostname", help="Grid Master IP")
 parser.add_argument("-u", "--user")
 parser.add_argument("-p", "--password")
-parser.add_argument("-g", "--get", help="Get Current ADP Profile")
+parser.add_argument("-g", "--get", action="store_true", help="Get Current ADP Profile")
 parser.add_argument("-c", "--create", help="Create ADP Profile")
 parser.add_argument("-m", "--members", help="Members to assign to ADP Profile")
 parser.add_argument("-r", "--remove", help="Remove ADP Profile")
-parser.add_argument("-n", "--name", help="Name of ADP Profile to Add/Remove")
 parser.add_argument("-d", "--debug", action="store_true", help="Enable Debug Mode")
 args = parser.parse_args()
 # for debugging
@@ -34,21 +33,54 @@ opts = {"host": args.gmhostname, "username": args.user, "password": args.passwor
 conn = connector.Connector(opts)
 # get current profiles
 if args.get:
-	existing_adp_profiles = conn.get_object("threatprotection:profile")
-# print network view
-if args.debug:
-	print(existing_adp_profiles)
-if existing_adp_profiles:
-	for existing_profiles in existing_adp_profiles:
-		print(existing_profiles["name"])
-else:
-	print("No profiles configured")
-
+    existing_adp_profiles = conn.get_object("threatprotection:profile")
+    # print network view
+    if args.debug:
+        print(existing_adp_profiles)
+    if existing_adp_profiles:
+        for existing_profiles in existing_adp_profiles:
+            print(existing_profiles)
+    else:
+        print("No profiles configured")
+# create adp profiles
 if args.create:
-	if args.name:
-		if args.members:
-			new_adp_profile = conn.create_object("threatprotection:profile", name=args.name, members=[args.members], use_current_ruleset=True)
-		else:
-			new_adp_profile = conn.create_object("threatprotection:profile", name=args.name, use_current_ruleset=True)
-	else:
-		print("No name assigned")
+    ruleset = conn.get_object(
+        "grid:threatprotection", return_fields=["current_ruleset"]
+    )
+    if args.members:
+        new_adp_profile = conn.create_object(
+            "threatprotection:profile",
+            {
+                "name": args.create,
+                "members": [args.members],
+                "use_current_ruleset": True,
+                "current_ruleset": ruleset[0]["current_ruleset"],
+            },
+        )
+    else:
+        new_adp_profile = conn.create_object(
+            "threatprotection:profile",
+            {
+                "name": args.create,
+                "use_current_ruleset": True,
+                "current_ruleset": ruleset[0]["current_ruleset"],
+            },
+        )
+    if new_adp_profile:
+        if args.debug:
+            print(new_adp_profile)
+        print("ADP Profile created: {}".format(new_adp_profile))
+    else:
+        print("Error creating profile")
+
+# remove adp profiles
+if args.remove:
+    adp_profile = conn.get_object("threatprotection:profile", {"name": args.remove})
+    if adp_profile:
+        del_adp_profile = conn.delete_object(adp_profile[0]["_ref"])
+        if del_adp_profile:
+            print("ADP profile {} removed".format(args.remove))
+        else:
+            print("ADP profile {} removal failed".format(args.remove))
+    else:
+        print("ADP Profile not found")
